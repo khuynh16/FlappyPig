f144b7180fd934f97c27a91ba40a149835ac7bd9
Kelvin Huynh <kelvinahuynh@gmail.com>
2020-10-27 17:06:09 -0400

    cleaned and added comments

--- script.js
+++ script.js
@@ -3,21 +3,24 @@ let lowerObstacle = document.getElementById('lowerObstacle');
 let startButton = document.getElementById('startButton');
 let piggy = document.getElementById('piggyCharacter');
 let htmlPage = document.getElementById('html');
-var jumping = 0;                                            // holds jumping action (true or false)
-var counter = 0;
 
+// holds jumping action (true or false; false is 0, 1 is true)
+let jumping = 0;       
+
+// runs the below code when animation repeats
+// changes the lengths of the obstacles with a space big enough for piggy to fit through
 upperObstacle.addEventListener('animationiteration', () => {
     let upperObsHeight = Math.floor((Math.random()*61)) + 10;
     let lowerObsHeight = 70 - upperObsHeight;
-    // console.log(upperObsHeight);
-    // console.log(lowerObsHeight);
+
     let upperRand = Math.floor(Math.random()*upperObsHeight + 1);
     let lowerRand = Math.floor(Math.random()*lowerObsHeight + 1);
+
     upperObstacle.style.height = upperRand + 'vh';
     lowerObstacle.style.height = lowerRand + 'vh';
-    counter++;
 });
 
+// calls when user click start game button
 function startGame() {
     htmlPage.setAttribute('onclick', 'jump()');
     upperObstacle.classList.add('animationClass');
@@ -25,15 +28,14 @@ function startGame() {
     startButton.style.display = 'none';
 
     setInterval(function(){
-        var piggyTop = parseInt(window.getComputedStyle(piggy).getPropertyValue("top"));
-        // console.log('property value: ' + piggy.style.top);
+        // current piggy top in pixels
+        let piggyTop = parseInt(window.getComputedStyle(piggy).getPropertyValue("top"));
 
-        // get pixel numberic value of piggy style top
+        // top value of piggy in pixels
         let value = parseInt(piggy.style.top.substring(0, piggy.style.top.length - 2)) * 100;
         // cross-browser @media (width) and @media (height) values 
         const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
-
-        // create the vh value
+        // create the vh value of current piggy location on page
         let calculatedVh = value / vh;
 
         if(jumping==0){
@@ -42,37 +44,36 @@ function startGame() {
                 piggy.style.top = (piggyTop+3)+"px";
         }
 
-        var upperBoundary = upperObstacle.getBoundingClientRect();
-        var lowerBoundary = lowerObstacle.getBoundingClientRect();
-        var piggyBoundary = piggy.getBoundingClientRect();
+        // boundary values of the objects
+        let upperBoundary = upperObstacle.getBoundingClientRect();
+        let lowerBoundary = lowerObstacle.getBoundingClientRect();
+        let piggyBoundary = piggy.getBoundingClientRect();
 
+        // cases where piggy hits top obstacle
         if (upperBoundary.left > 0 && (upperBoundary.left <= piggyBoundary.right) && (piggyBoundary.top + 20 < upperBoundary.bottom) && (upperBoundary.right > piggyBoundary.left)) {
             alert('Game over! Hit top obstacle!');
         }
 
+        // cases where piggy hits bottom obstacle
         if (lowerBoundary.left > 0 && (lowerBoundary.left <= piggyBoundary.right) && (piggyBoundary.bottom > lowerBoundary.top + 16) && (lowerBoundary.right > piggyBoundary.left)) {
             alert('Game over! Hit bottom obstacle!');
         }
     },10);
 }
 
+// jump animation for piggy character
 function jump(){
-    // variable to mean currently jumping
-    jumping = 1;
-    let jumpCount = 0;
-    var jumpInterval = setInterval(function(){
-        var piggyTop = parseInt(window.getComputedStyle(piggy).getPropertyValue("top"));
-        // console.log('piggyHeight: ' + piggyTop);
-        // if((characterTop>6)&&(jumpCount<15)){
-            piggy.style.top = (piggyTop-5)+"px";
-        // }
-        // console.log(jumpCount);
+    jumping = 1;        // variable to mean currently jumping
+    let jumpCount = 0;  // interval count; goes to 20 to enhance jumping animation
+
+    let jumpInterval = setInterval(function(){
+        let piggyTop = parseInt(window.getComputedStyle(piggy).getPropertyValue("top"));
+        piggy.style.top = (piggyTop-5)+"px";
         if(jumpCount> 20 || piggyTop < 0){
             // stops interval from increasing height of jump
             clearInterval(jumpInterval);
             //need to reset and allow gravity
             jumping=0;
-            // jumpCount=0;
         }
         jumpCount++;
     },10);
